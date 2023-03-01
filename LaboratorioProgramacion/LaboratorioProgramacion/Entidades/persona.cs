78fa0c1f75a57c5402cb589c3b73fb9a2c5610d1
KimberliFlores <fp0204042022@unab.edu.sv>
2023-03-01 07:28:31 -0600

    hello

---
+++ LaboratorioProgramacion/LaboratorioProgramacion/Entidades/persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioProgramacion.Entidades
+{
+    public class persona
+    {
+        public String Nombre { get; set; }
+
+        public int Edad { get; set; }
+
+        public String Sexo { get; set; }
+
+        public double Peso { get; set; }
+
+        public double Altura { get; set; }
+    }
+}
+  
---
+++ LaboratorioProgramacion/LaboratorioProgramacion/Negocio/clspersona.cs
@@ -0,0 +1,45 @@
+﻿using LaboratorioProgramacion.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioProgramacion.Negocio
+{
+    public class clspersona
+    {
+        public void calcularIMC(persona info)
+        {
+            double pro = info.Peso / Math.Pow(info.Altura, 2);
+            if (pro < 24)
+            {
+                Console.WriteLine("Peso ideal!");
+            }
+            else
+            {
+
+                Console.WriteLine("Tiene sobre peso");
+            }
+
+
+
+        }
+        public void esMayorDeEdad(persona info)
+        {
+            int edad = info.Edad;
+            if (edad > 18)
+            {
+                Console.WriteLine("Es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Es menor de edad");
+            }
+        }
+    }
+    } 
+
+        
+
+
